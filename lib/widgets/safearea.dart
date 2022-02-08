a916fcc1cae8392cbc257a28e3fb47f2cccbed22
onishi taisei <taisei.onishi@axia-works.com>
2022-02-09 00:01:27 +0900

    SafeAreaについてアウトプット

--- lib/widgets/safearea.dart
+++ lib/widgets/safearea.dart
@@ -4,7 +4,21 @@ class SafeAreaPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            padding: const EdgeInsets.all(5.0),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.red),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(border: InputBorder.none),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
